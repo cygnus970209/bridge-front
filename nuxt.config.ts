55ea48344f886143aeb016952fde54fb91db8147
EUN-HYE-KIM <dmsp1234@gmail.com>
2024-06-03 15:10:42 +0900

    nuxt.config 수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from 'url';
+
 export default defineNuxtConfig({
+  ssr: true,
+  app: {
+    head: {
+      title: 'bridge',
+      htmlAttrs: {
+        lang: 'ko',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width,initial-scale=1, maximum-scale=1.0, user-scalable=no' },
+      ],
+    },
+  },
   devtools: { enabled: true },
+  alias: {
+    '@': fileURLToPath(new URL('./src/', import.meta.url)),
+    '~': fileURLToPath(new URL('./', import.meta.url)),
+  },
+  components: {
+    dirs: [
+      {
+        path: '@/components',
+        extensions: ['vue'],
+        global: true,
+      },
+    ],
+  },
+  css: ['@/assets/css/main.scss'],
+  buildDir: 'dist',
+  srcDir: 'src/',
+  sourcemap: {
+    server: true,
+    client: false,
+  },
 });
