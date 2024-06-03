be0b10ee63bffc62cac4e796cd96c735897a2a2c
EUN-HYE-KIM <dmsp1234@gmail.com>
2024-06-03 23:12:31 +0900

    BaseButton 컴포넌트 추가

---
+++ components/atom/BaseButton.vue
@@ -0,0 +1,25 @@
+<template>
+	<button type="button" :disabled="props.isDisabled" @click="click" class="btn__item">
+		<img v-if="props.iconSrc" :src="props.iconSrc" alt="버튼 아이콘" class="btn__icon">
+		<span v-if="props.btnText" v-html="props.btnText" class="btn__text"></span>
+	</button>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+	isDisabled: {
+		type: Boolean,
+		default: false
+	},
+	iconSrc: String,
+	btnText: [String, Number]
+});
+
+// 이벤트 정의
+const emit = defineEmits(['click']);
+const click = () => {
+	emit('click');
+};
+</script>
