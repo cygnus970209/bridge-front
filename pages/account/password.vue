c0f700901da8314d6a601b3c84033157ee47bdc4
EUN-HYE-KIM <dmsp1234@gmail.com>
2024-06-18 01:36:42 +0900

    회원가입 > 약관 퍼블리싱

--- pages/account/password.vue
+++ pages/account/password.vue
@@ -1,16 +1,16 @@
 <template>
-  <section class="section">
-      <div class="section__top">
-          <h2 class="section__title">title</h2>
-          <div class="input__container">
-              <MoeculeBaseInput type="password"></MoeculeBaseInput>
-              <MoeculeBaseInput type="password"></MoeculeBaseInput>
-          </div>
-      </div>
-      <div class="btn__container btn__container--column">
-          <MoeculeOneButton btnText="다음"></MoeculeOneButton>
-      </div>
-  </section>
+	<section class="section">
+		<div class="section__top">
+			<h2 class="section__title">title</h2>
+			<div class="input__container">
+				<MoeculeBaseInput type="password"></MoeculeBaseInput>
+				<MoeculeBaseInput type="password"></MoeculeBaseInput>
+			</div>
+		</div>
+		<div class="btn__container btn__container--column">
+			<MoeculeOneButton btnText="다음"></MoeculeOneButton>
+		</div>
+	</section>
 </template>
 
 <script setup>
--- pages/account/terms.vue
+++ pages/account/terms.vue
@@ -1,22 +1,40 @@
 <template>
-    <section class="section">
-        <div class="section__top">
-            <h2 class="section__title">이용약관 동의</h2>
-            <label v-for="item in terms" class="checkbox">
-                <input type="checkbox" :value="item.value" v-model="checkedTerms" class="checkbox__item">{{ item.text }}
-            </label>
-        </div>
-        <MoeculeOneButton btnText="다음"></MoeculeOneButton>
-    </section>
+	<section class="section">
+		<div class="section__top">
+			<h2 class="section__title">이용약관 동의</h2>
+			<label class="checkbox checkbox--all" @click="isChecked">
+				<input type="checkbox" value="all" v-model="checkedTerms" class="checkbox__item"><span
+					class="checkbox__text">네,
+					모두 동의
+					합니다.</span>
+			</label>
+			<div class="checkbox__list">
+				<label v-for="item in terms" :class="['checkbox', item.isRequired ? 'checkbox--required' : '']">
+					<input type="checkbox" :value="item.value" v-model="checkedTerms" class="checkbox__item"><span
+						class="checkbox__text">{{
+				item.text }}&nbsp;<strong>({{ item.isRequired ? '필수' : '선택' }})</strong></span>
+				</label>
+			</div>
+		</div>
+		<MoeculeOneButton btnText="다음"></MoeculeOneButton>
+	</section>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
 const terms = ref([
-    { isRequired: false, value: 'all', text: '네, 모두 동의 합니다.' },
-    { isRequired: true, value: 'collection', text: '이용약관' },
-    { isRequired: true, value: 'thirdParty', text: '개인정보 수집 및 이용 동의 ' },
+	{ isRequired: true, value: 'collection', text: '이용약관' },
+	{ isRequired: true, value: 'thirdParty', text: '개인정보 수집 및 이용 동의 ' },
 ]);
 const checkedTerms = ref([]);
+const isChecked = () => {
+	const checkAll = document.querySelector('.checkbox--all');
+	const checkbox = document.querySelector('.checkbox--all > input');
+
+	if (checkbox.checked) {
+		checkAll.classList.toggle('checkbox--checked');
+	}
+}
+
 </script>
